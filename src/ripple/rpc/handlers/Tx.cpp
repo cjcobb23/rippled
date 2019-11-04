76593d44b2c79f9388d6d3bb38a7bc4b2ea7b4f4
CJ Cobb <ccobb@ripple.com>
2019-11-04 14:45:26 -0800

    basic tx command

--- src/ripple/rpc/handlers/Tx.cpp
+++ src/ripple/rpc/handlers/Tx.cpp
@@ -29,6 +29,8 @@
 #include <ripple/rpc/DeliveredAmount.h>
 #include <ripple/rpc/impl/RPCHelpers.h>
 
+#include <ripple/rpc/GRPCHandlers.h>
+
 namespace ripple {
 
 // {
@@ -53,16 +55,26 @@ isHexTxID (std::string const& txid)
 
 static
 bool
+isValidated(LedgerMaster& ledgerMaster, std::uint32_t seq, uint256 const& hash)
+{
+    if (!ledgerMaster.haveLedger (seq))
+        return false;
+
+    if (seq > ledgerMaster.getValidatedLedger ()->info().seq)
+        return false;
+
+    return ledgerMaster.getHashBySeq (seq) == hash;
+
+}
+
+static
+bool
 isValidated (RPC::Context& context, std::uint32_t seq, uint256 const& hash)
 {
-    if (!context.ledgerMaster.haveLedger (seq))
-        return false;
-
-    if (seq > context.ledgerMaster.getValidatedLedger ()->info().seq)
-        return false;
-
-    return context.ledgerMaster.getHashBySeq (seq) == hash;
+    return isValidated(context.ledgerMaster, seq, hash);
 }
+
+
 
 bool
 getMetaHex (Ledger const& ledger,
@@ -142,6 +154,101 @@ Json::Value doTx (RPC::Context& context)
     }
 
     return ret;
+}
+
+void populateTransaction(
+        io::xpring::Transaction* txn_proto,
+        std::shared_ptr<STTx const> txn_st)
+{
+    STAmount fee = txn_st->getFieldAmount(sfFee);
+    txn_proto->mutable_fee()->set_drops(std::to_string(fee.xrp().drops()));
+    //TODO: what about when the amount is not xrp?
+    //What does the data look like then
+    STAmount amount = txn_st->getFieldAmount(sfAmount);
+    txn_proto->mutable_payment()->mutable_xrp_amount()->set_drops(std::to_string(amount.xrp().drops()));
+
+
+    //TODO return account as bytes or base58 string?
+    AccountID account = txn_st->getAccountID(sfAccount);
+    const char* account_data = reinterpret_cast<const char*>(account.data());
+
+    std::string account_str{account_data,account.size()};
+
+    //std::cout << "account is " << toBase58(account) << std::endl;
+
+    txn_proto->set_account(account_str);
+
+    AccountID account_dest = txn_st->getAccountID(sfDestination);
+    const char* account_dest_data = reinterpret_cast<const char*>(account_dest.data());
+
+
+    std::string account_dest_str{account_dest_data,account_dest.size()};
+    txn_proto->mutable_payment()->set_destination(account_dest_str);
+    txn_proto->set_sequence(txn_st->getFieldU32(sfSequence));
+    Blob signingPubKey = txn_st->getFieldVL(sfSigningPubKey);
+    std::string pubKeyStr{reinterpret_cast<const char*>(signingPubKey.data()),signingPubKey.size()};
+    txn_proto->set_signing_public_key_hex(pubKeyStr);
+}
+
+std::pair<io::xpring::TxResponse, grpc::Status>
+doTxGrpc(RPC::ContextGeneric<io::xpring::TxRequest>& context)
+{
+    io::xpring::TxResponse result;
+    grpc::Status status = grpc::Status::OK;
+
+    io::xpring::TxRequest& request = context.params;
+
+    std::string const& hash_bytes = request.hash();
+
+    uint256 hash = uint256::fromVoid(hash_bytes.data());
+    std::cout << "hash is " << to_string(hash) << std::endl;
+
+    std::shared_ptr<Transaction> txn = context.app.getMasterTransaction().fetch(hash, true);
+
+    if(!txn)
+    {
+        std::cout << "txn not found" << std::endl;
+        return {result,status};
+        // return not found
+    }
+    std::shared_ptr<STTx const> st_txn = txn->getSTransaction();
+
+/*    for(size_t i = 0; i < st_txn->getCount(); ++i)
+    {
+        SField const& field = st_txn->getFieldSType(i);
+        std::cout << "field name is " << field.getName();
+        
+        std::cout << st_txn->peekAtField(field).getFullText() << std::endl;
+    }
+*/
+
+    populateTransaction(result.mutable_tx(),st_txn);
+
+    result.set_ledger_index(txn->getLedger());
+
+    std::shared_ptr<Ledger const> ledger = 
+        context.ledgerMaster.getLedgerBySeq(txn->getLedger());
+    if(ledger)
+    { 
+        auto rawMeta = ledger->txRead (txn->getID()).second;
+        if(rawMeta)
+        {
+
+            auto txMeta = std::make_shared<TxMeta>(
+                    txn->getID(), ledger->seq(), *rawMeta);
+
+            result.mutable_meta()->set_transaction_index(txMeta->getIndex());
+            result.mutable_meta()->set_transaction_result(transToken(txMeta->getResultTER()));
+            bool validated = isValidated(context.ledgerMaster,
+                    ledger->info().seq,ledger->info().hash);
+            std::cout << "validated is " << validated << std::endl;
+            //TODO is this in the output when false?
+            result.set_validated(validated);
+        }
+    }
+
+    return {result, status};
+
 }
 
 } // ripple
