1a35a88c50c0182eea9e8af041ce95dbe28edf42
CJ Cobb <ccobb@ripple.com>
2019-11-06 16:07:25 -0800

    lots more data for tx

--- src/ripple/rpc/handlers/Tx.cpp
+++ src/ripple/rpc/handlers/Tx.cpp
@@ -156,6 +156,13 @@ Json::Value doTx (RPC::Context& context)
     return ret;
 }
 
+template <class T>
+std::string toBytes(T& data)
+{
+    const char* bytes = reinterpret_cast<const char*>(data.data());
+    return {bytes,data.size()};
+}
+
 void populateTransaction(
         io::xpring::Transaction* txn_proto,
         std::shared_ptr<STTx const> txn_st)
@@ -165,7 +172,25 @@ void populateTransaction(
     //TODO: what about when the amount is not xrp?
     //What does the data look like then
     STAmount amount = txn_st->getFieldAmount(sfAmount);
-    txn_proto->mutable_payment()->mutable_xrp_amount()->set_drops(std::to_string(amount.xrp().drops()));
+    if(amount.native())
+    {
+    
+        txn_proto->mutable_payment()->mutable_xrp_amount()->set_drops(std::to_string(amount.xrp().drops()));
+    }
+    else
+    {
+        txn_proto->mutable_payment()->mutable_fiat_amount()->set_value(to_string(amount.iou()));
+        txn_proto->mutable_payment()->mutable_fiat_amount()->set_issuer(toBase58(amount.issue().account));
+        io::xpring::Currency* currency_proto = txn_proto->mutable_payment()->mutable_fiat_amount()->mutable_currency();
+        Currency currency = amount.issue().currency;
+        currency_proto->set_name(to_string(currency));
+
+        const char* currency_bytes = reinterpret_cast<const char*>(currency.data());
+        std::string currency_str{currency_bytes,currency.size()};
+
+        currency_proto->set_code(currency_str);
+
+    }
 
 
     //TODO return account as bytes or base58 string?
@@ -176,18 +201,101 @@ void populateTransaction(
 
     //std::cout << "account is " << toBase58(account) << std::endl;
 
-    txn_proto->set_account(account_str);
+    txn_proto->set_account(toBase58(account));
 
     AccountID account_dest = txn_st->getAccountID(sfDestination);
     const char* account_dest_data = reinterpret_cast<const char*>(account_dest.data());
 
 
     std::string account_dest_str{account_dest_data,account_dest.size()};
-    txn_proto->mutable_payment()->set_destination(account_dest_str);
+    txn_proto->mutable_payment()->set_destination(toBase58(account_dest));
+
     txn_proto->set_sequence(txn_st->getFieldU32(sfSequence));
+
     Blob signingPubKey = txn_st->getFieldVL(sfSigningPubKey);
     std::string pubKeyStr{reinterpret_cast<const char*>(signingPubKey.data()),signingPubKey.size()};
     txn_proto->set_signing_public_key_hex(pubKeyStr);
+
+    txn_proto->set_flags(txn_st->getFieldU32(sfFlags));
+
+    txn_proto->set_last_ledger_sequence(txn_st->getFieldU32(sfLastLedgerSequence));
+
+    STPathSet const & pathset = txn_st->getFieldPathSet(sfPaths);
+
+    for(auto it = pathset.begin(); it < pathset.end(); ++it)
+    {
+        STPath const & path = *it;
+
+        io::xpring::Path* proto_path = txn_proto->add_paths();
+
+        for(auto it2 = path.begin(); it2 != path.end(); ++it2)
+        {
+            io::xpring::PathElement* proto_element = proto_path->add_elements();
+
+            STPathElement const & elt = *it2;
+        
+            if(elt.isOffer())
+            {
+                if(elt.hasCurrency())
+                {
+                   Currency const & currency = elt.getCurrency(); 
+                   proto_element->set_currency(to_string(currency));
+                }
+                if(elt.hasIssuer())
+                {
+                    AccountID const & issuer = elt.getIssuerID();
+                    proto_element->set_issuer(toBase58(issuer));
+                }
+            }
+            else
+            {
+                AccountID const & path_account = elt.getAccountID();
+                proto_element->set_account(toBase58(path_account));
+            
+            }
+        }
+    }
+
+   STAmount const & send_max = txn_st->getFieldAmount(sfSendMax);
+
+
+   if(send_max.native())
+   {
+       //TODO: shouldnt happen
+       
+       std::cout << "send max is native" << std::endl;
+
+        
+
+       io::xpring::XRPAmount* native_send_max = txn_proto->mutable_send_max_native();
+       native_send_max->set_drops(std::to_string(send_max.xrp().drops()));
+    
+   }
+   else
+   {
+    Issue const & issue = send_max.issue();
+
+    io::xpring::FiatAmount* fiat_send_max = txn_proto->mutable_send_max_fiat();
+    fiat_send_max->set_issuer(toBase58(issue.account));
+    io::xpring::Currency* currency_proto = fiat_send_max->mutable_currency();
+    Currency currency = issue.currency;
+    currency_proto->set_name(to_string(currency));
+
+    const char* currency_bytes = reinterpret_cast<const char*>(currency.data());
+    std::string currency_str{currency_bytes,currency.size()};
+
+    currency_proto->set_code(currency_str);
+
+    fiat_send_max->set_value(to_string(send_max.iou()));
+   }
+
+   Blob blob = txn_st->getFieldVL(sfTxnSignature);
+
+
+   txn_proto->set_signature(toBytes(blob));
+
+
+
 }
 
 std::pair<io::xpring::TxResponse, grpc::Status>
@@ -244,6 +352,78 @@ doTxGrpc(RPC::ContextGeneric<io::xpring::TxRequest>& context)
             std::cout << "validated is " << validated << std::endl;
             //TODO is this in the output when false?
             result.set_validated(validated);
+
+            STArray& nodes = txMeta->getNodes();
+
+            for(auto it = nodes.begin(); it != nodes.end(); ++it)
+            {
+               STObject & obj = *it;
+               std::cout << "processing node" << std::endl;
+                for(size_t i = 0; i < obj.getCount(); ++i)
+                {
+                    SField const& field = obj.getFieldSType(i);
+                    std::cout << "field name is " << field.getName() << std::endl;
+                    
+                    std::cout << obj.peekAtField(field).getFullText() << std::endl;
+                }
+                std::cout << "fname is " << obj.getFName().getName() << std::endl;
+               if(obj.getFName() == sfModifiedNode)
+               {
+                   std::cout << "modified is present" << std::endl;
+                   io::xpring::AffectedNode* node = result.mutable_meta()->add_nodes();
+
+                   std::uint16_t type = obj.getFieldU16(sfLedgerEntryType);
+                   if(type == LedgerEntryType::ltACCOUNT_ROOT)
+                   {
+                     
+                       node->set_ledger_entry_type("AccountRoot");
+                       uint256 ledger_index = obj.getFieldH256(sfLedgerIndex);
+                       node->set_ledger_index(toBytes(ledger_index));
+
+                   }
+                   if(obj.isFieldPresent(sfFinalFields))
+                   {
+                       std::cout << "final fields is present" << std::endl;
+
+                   STObject& final_fields = obj.getField(sfFinalFields).downcast<STObject>();
+                   io::xpring::LedgerFields* final_fields_proto = node->mutable_modified_node()->mutable_final_fields();
+
+                   final_fields_proto->set_account(toBase58(final_fields.getAccountID(sfAccount)));
+                   final_fields_proto->set_balance(final_fields.getFieldAmount(sfBalance).xrp().drops());
+                   final_fields_proto->set_sequence(final_fields.getFieldU32(sfSequence));
+                   }
+                   if(obj.isFieldPresent(sfPreviousFields))
+                   {
+                       std::cout << "previous fields is present" << std::endl;
+                        STObject& prev_fields = obj.getField(sfPreviousFields).downcast<STObject>();
+
+                        io::xpring::LedgerFields* prev_fields_proto =
+                            node->mutable_modified_node()->mutable_previous_fields();
+
+                        prev_fields_proto->set_balance(prev_fields.getFieldAmount(sfBalance).xrp().drops());
+                        prev_fields_proto->set_sequence(prev_fields.getFieldU32(sfSequence));
+                        //prev_fields_proto->set_account(toBase58(prev_fields.getAccountID(sfAccount)));
+
+                   }
+                   std::cout << "setting prev txn id and sequence" << std::endl;
+                   uint256 prev_txn_id = obj.getFieldH256(sfPreviousTxnID);
+                   node->mutable_modified_node()->set_previous_txn_id(toBytes(prev_txn_id));
+                   node->mutable_modified_node()->set_ledger_index(obj.getFieldU32(sfPreviousTxnLgrSeq));
+
+                   std::cout << "done" << std::endl;
+
+
+
+
+
+
+
+               }
+
+              
+
+            }
+
         }
     }
 
