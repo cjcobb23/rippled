307378d23d22c4a439030993a27a8ec0e283ea4c
CJ Cobb <ccobb@ripple.com>
2020-01-24 14:07:17 -0800

    serialize all ledger objects to protobuf

--- src/ripple/rpc/impl/GRPCHelpers.h
+++ src/ripple/rpc/impl/GRPCHelpers.h
@@ -44,11 +44,31 @@ populateProtoU8(STObject const& obj, SF_U8 const& field, T const& getProto)
 
 template <class T>
 void
+populateProtoU16(STObject const& obj, SF_U16 const& field, T const& getProto)
+{
+    if (obj.isFieldPresent(field))
+    {
+        getProto()->set_value(obj.getFieldU16(field));
+    }
+}
+
+template <class T>
+void
 populateProtoU32(STObject const& obj, SF_U32 const& field, T const& getProto)
 {
     if (obj.isFieldPresent(field))
     {
         getProto()->set_value(obj.getFieldU32(field));
+    }
+}
+
+template <class T>
+void
+populateProtoU64(STObject const& obj, SF_U64 const& field, T const& getProto)
+{
+    if (obj.isFieldPresent(field))
+    {
+        getProto()->set_value(obj.getFieldU64(field));
     }
 }
 
@@ -88,6 +108,17 @@ populateProtoH128(STObject const& obj, SF_U128 const& field, T const& getProto)
     }
 }
 
+
+template <class T>
+void
+populateProtoH160(STObject const& obj, SF_U160 const& field, T const& getProto)
+{
+    if (obj.isFieldPresent(field))
+    {
+        getProto()->set_value(obj.getFieldH160(field).data(), uint160::size());
+    }
+}
+
 template <class T>
 void
 populateProtoH256(STObject const& obj, SF_U256 const& field, T const& getProto)
@@ -95,6 +126,24 @@ populateProtoH256(STObject const& obj, SF_U256 const& field, T const& getProto)
     if (obj.isFieldPresent(field))
     {
         getProto()->set_value(obj.getFieldH256(field).data(), uint256::size());
+    }
+}
+
+template <class T>
+void
+populateProtoVec256(
+    STObject const& obj,
+    SF_Vec256 const& field,
+    T const& getProto)
+{
+    if (obj.isFieldPresent(field))
+    {
+        const STVector256& vec = obj.getFieldV256(field);
+        for (size_t i = 0; i < vec.size(); ++i)
+        {
+            uint256 const& elt = vec[i];
+            getProto()->set_value(elt.data(), elt.size());
+        }
     }
 }
 
@@ -488,10 +537,6 @@ void populateMemoData(STObject const& obj, T& proto)
     });
 }
 
-
-
-
-
 template <class T>
 void populateMemoFormat(STObject const& obj, T& proto)
 {
@@ -516,6 +561,97 @@ void populateSequence(STObject const& obj, T& proto)
     });
 }
 
+
+template <class T, class R>
+void
+populateProtoArray(
+    STObject const& obj,
+    SField const& outerField,
+    SField const& innerField,
+    T const& getProto,
+    R const& populateProto)
+{
+    std::cout << "populate proto array" << std::endl;
+    if (obj.isFieldPresent(outerField) &&
+        obj.peekAtField(outerField).getSType() == SerializedTypeID::STI_ARRAY)
+    {
+        std::cout << "got outer field" << std::endl;
+        auto arr = obj.getFieldArray(outerField);
+        for (auto it = arr.begin(); it != arr.end(); ++it)
+        {
+            std::cout << "looping" << std::endl;
+            std::cout << it->getFullText() << std::endl;
+            std::cout << it->isFieldPresent(innerField) << std::endl;
+            populateProto(*it, *getProto());
+//            if (it->isFieldPresent(innerField) &&
+//                it->peekAtField(innerField).getSType() == SerializedTypeID::STI_OBJECT)
+//            {
+//                std::cout << "got inner object" << std::endl;
+//                auto proto = getProto();
+//                auto innerObj = it->getField(innerField).downcast<STObject>();
+//                populateProto(innerObj, *proto);
+//            }
+        }
+    }
+}
+
+
+
+template <class T>
+void populateSignerWeight(STObject const& obj, T& proto)
+{
+    populateProtoU16(obj, sfSignerWeight, [&proto]() {
+        return proto.mutable_signer_weight();
+    });
+}
+
+template <class T>
+void
+populateSignerEntries(STObject const& obj, T& proto)
+{
+    populateProtoArray(
+        obj,
+        sfSignerEntries,
+        sfSignerEntry,
+        [&proto]() { return proto.add_signer_entries(); },
+        [](auto& innerObj, auto& innerProto) {
+            populateAccount(innerObj, innerProto);
+            populateSignerWeight(innerObj, innerProto);
+        });
+}
+
+template <class T>
+void
+populateMemos(STObject const& obj, T& proto)
+{
+    populateProtoArray(
+        obj,
+        sfMemos,
+        sfMemo,
+        [&proto]() { return proto.add_memos(); },
+        [](auto& innerObj, auto& innerProto) {
+            populateMemoData(innerObj, innerProto);
+            populateMemoType(innerObj, innerProto);
+            populateMemoFormat(innerObj, innerProto);
+        });
+}
+
+template <class T>
+void
+populateSigners(STObject const& obj, T& proto)
+{
+    populateProtoArray(
+        obj,
+        sfSigners,
+        sfSigner,
+        [&proto]() { return proto.add_signers(); },
+        [](auto& innerObj, auto& innerProto) {
+            populateAccount(innerObj, innerProto);
+            populateTransactionSignature(innerObj, innerProto);
+            populateSigningPublicKey(innerObj, innerProto);
+        });
+}
+/*
 template <class T>
 void
 populateMemos(STObject const& obj, T& proto)
@@ -559,6 +695,344 @@ populateSigners(STObject const& obj, T& proto)
             }
         }
     }
+}
+*/
+
+
+
+template <class T>
+void populateOwnerCount(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfOwnerCount, [&proto]() {
+        return proto.mutable_owner_count();
+    });
+}
+
+
+template <class T>
+void populatePreviousTransactionID(STObject const& obj, T& proto)
+{
+    populateProtoH256(obj, sfPreviousTxnID, [&proto]() {
+        return proto.mutable_previous_transaction_id();
+    });
+}
+
+
+template <class T>
+void populatePreviousTransactionLedgerSequence(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfPreviousTxnLgrSeq, [&proto]() {
+        return proto.mutable_previous_transaction_ledger_sequence();
+    });
+}
+
+
+template <class T>
+void populateLowLimit(STObject const& obj, T& proto)
+{
+    populateProtoAmount(obj, sfLowLimit, [&proto]() {
+        return proto.mutable_low_limit();
+    });
+}
+
+
+template <class T>
+void populateHighLimit(STObject const& obj, T& proto)
+{
+    populateProtoAmount(obj, sfHighLimit, [&proto]() {
+        return proto.mutable_high_limit();
+    });
+}
+
+
+template <class T>
+void populateLowNode(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfLowNode, [&proto]() {
+        return proto.mutable_low_node();
+    });
+}
+
+
+template <class T>
+void populateHighNode(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfHighNode, [&proto]() {
+        return proto.mutable_high_node();
+    });
+}
+
+
+
+template <class T>
+void populateLowQualityIn(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfLowQualityIn, [&proto]() {
+        return proto.mutable_low_quality_in();
+    });
+}
+
+
+template <class T>
+void populateLowQualityOut(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfLowQualityOut, [&proto]() {
+        return proto.mutable_low_quality_out();
+    });
+}
+
+
+template <class T>
+void populateHighQualityIn(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfHighQualityIn, [&proto]() {
+        return proto.mutable_high_quality_in();
+    });
+}
+
+template <class T>
+void populateHighQualityOut(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfHighQualityOut, [&proto]() {
+        return proto.mutable_high_quality_out();
+    });
+}
+
+
+
+
+template <class T>
+void populateBookDirectory(STObject const& obj, T& proto)
+{
+    populateProtoH256(obj, sfBookDirectory, [&proto]() {
+        return proto.mutable_book_directory();
+    });
+}
+
+
+template <class T>
+void populateBookNode(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfBookNode, [&proto]() {
+        return proto.mutable_book_node();
+    });
+}
+
+
+template <class T>
+void populateOwnerNode(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfOwnerNode, [&proto]() {
+        return proto.mutable_owner_node();
+    });
+}
+
+
+
+template <class T>
+void populateSignerListID(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfSignerListID, [&proto]() {
+        return proto.mutable_signer_list_id();
+    });
+}
+
+
+template <class T>
+void populateAmendment(STObject const& obj, T& proto)
+{
+    populateProtoH256(obj, sfAmendment, [&proto]() {
+        return proto.mutable_amendment();
+    });
+}
+
+
+template <class T>
+void populateCloseTime(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfCloseTime, [&proto]() {
+        return proto.mutable_close_time();
+    });
+}
+
+
+template <class T>
+void
+populateMajorities(STObject const& obj, T& proto)
+{
+    populateProtoArray(
+        obj,
+        sfMajorities,
+        sfMajority,
+        [&proto]() { return proto.add_majorities(); },
+        [](auto innerObj, auto innerProto) {
+            populateAmendment(innerObj, innerProto);
+            populateCloseTime(innerObj, innerProto);
+        });
+}
+
+
+template <class T>
+void populateAmendments(STObject const& obj, T& proto)
+{
+    populateProtoVec256(obj, sfAmendments, [&proto]() {
+        return proto.add_amendments();
+    });
+}
+
+template <class T>
+void populateHashes(STObject const& obj, T& proto)
+{
+    populateProtoVec256(obj, sfHashes, [&proto]() {
+        return proto.add_hashes();
+    });
+}
+
+
+
+/*
+template <class T>
+void populateSignerEntries(STObject const& obj, T& proto)
+{
+    if (obj.isFieldPresent(sfSignerEntries))
+    {
+        STArray const& signerEntries = obj.getFieldArray(sfSignerEntries);
+
+        for (auto it = signerEntries.begin(); it != signerEntries.end(); ++it)
+        {
+            rpc::v1::SignerEntry& signerEntryProto =
+                *proto.add_signer_entries();
+
+            populateAccount(*it, signerEntryProto);
+            populateSignerWeight(*it, signerEntryProto);
+        }
+    }
+}
+*/
+template <class T>
+void
+populateIndexes(STObject const& obj, T& proto)
+{
+    populateProtoVec256(
+        obj, sfIndexes, [&proto]() { return proto.add_indexes(); });
+}
+
+template <class T>
+void populateRootIndex(STObject const& obj, T& proto)
+{
+    populateProtoH256(obj, sfRootIndex, [&proto]() {
+        return proto.mutable_root_index();
+    });
+}
+
+
+template <class T>
+void populateIndexNext(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfIndexNext, [&proto]() {
+        return proto.mutable_index_next();
+    });
+}
+
+
+template <class T>
+void populateIndexPrevious(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfIndexPrevious, [&proto]() {
+        return proto.mutable_index_previous();
+    });
+}
+
+template<class T>
+void populateProtoCurrency(STObject const& obj,SF_U160 const& field, T const& getProto)
+{
+    if(obj.isFieldPresent(field))
+    {
+        auto cur = obj.getFieldH160(field);
+        auto proto = getProto()->mutable_value();
+        proto->set_code(cur.data(), cur.size());
+        proto->set_name(to_string(cur));
+    }
+}
+
+template <class T>
+void populateTakerPaysCurrency(STObject const& obj, T& proto)
+{
+    populateProtoCurrency(obj, sfTakerPaysCurrency, [&proto]() {
+        return proto.mutable_taker_pays_currency();
+    });
+}
+
+
+template <class T>
+void populateTakerPaysIssuer(STObject const& obj, T& proto)
+{
+    populateProtoH160(obj, sfTakerPaysIssuer, [&proto]() {
+        return proto.mutable_taker_pays_issuer();
+    });
+}
+
+
+template <class T>
+void populateTakerGetsCurrency(STObject const& obj, T& proto)
+{
+    populateProtoCurrency(obj, sfTakerGetsCurrency, [&proto]() {
+        return proto.mutable_taker_gets_currency();
+    });
+}
+
+
+template <class T>
+void populateTakerGetsIssuer(STObject const& obj, T& proto)
+{
+    populateProtoH160(obj, sfTakerGetsIssuer, [&proto]() {
+        return proto.mutable_taker_gets_issuer();
+    });
+}
+
+
+template <class T>
+void populateDestinationNode(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfDestinationNode, [&proto]() {
+        return proto.mutable_destination_node();
+    });
+}
+
+
+template <class T>
+void populateBaseFee(STObject const& obj, T& proto)
+{
+    populateProtoU64(obj, sfBaseFee, [&proto]() {
+        return proto.mutable_base_fee();
+    });
+}
+
+
+template <class T>
+void populateReferenceFeeUnits(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfReferenceFeeUnits, [&proto]() {
+        return proto.mutable_reference_fee_units();
+    });
+}
+
+
+template <class T>
+void populateReserveBase(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfReserveBase, [&proto]() {
+        return proto.mutable_reserve_base();
+    });
+}
+
+
+template <class T>
+void populateReserveIncrement(STObject const& obj, T& proto)
+{
+    populateProtoU32(obj, sfReserveIncrement, [&proto]() {
+        return proto.mutable_reserve_increment();
+    });
 }
 
 /*
