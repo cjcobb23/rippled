e65d8d6c7eb5cf5f708e9bb82f9213090cf1b780
CJ Cobb <ccobb@ripple.com>
2020-01-28 15:09:03 -0800

    basic meta data testing

--- src/test/jtx/seq.h
+++ src/test/jtx/seq.h
@@ -57,6 +57,17 @@ public:
     operator()(Env&, JTx& jt) const;
 };
 
+struct last_ledger_seq
+{
+    private:
+        std::uint32_t num_;
+public:
+    explicit last_ledger_seq(std::uint32_t num) : num_(num) {}
+
+void
+operator()(Env&, JTx& jt) const;
+};
+
 } // jtx
 } // test
 } // ripple
