b41bf0ffdb2cf565a7d16e6de238aa5015cdff45
CJ Cobb <ccobb@ripple.com>
2020-09-25 13:06:46 -0400

    zlib

--- Builds/CMake/deps/cassandra.cmake
+++ Builds/CMake/deps/cassandra.cmake
@@ -10,30 +10,48 @@ find_library(cassandra NAMES cassandra REQUIRED)
 message(${cassandra})
 if(NOT cassandra)
 
-    find_library(krb5 NAMES krb5 krb5-dev libkrb5 libkrb5-dev REQUIRED)
-    add_library(krb5 SHARED IMPORTED GLOBAL)
-    ExternalProject_Add(krb5_src
-        PREFIX ${nih_cache_path}
-        GIT_REPOSITORY https://github.com/krb5/krb5.git
-        GIT_TAG master
-        CONFIGURE_COMMAND 
-        INSTALL_COMMAND ""
-        )
 
-    ExternalProject_Get_Property (krb5_src SOURCE_DIR)
-    ExternalProject_Get_Property (krb5_src BINARY_DIR)
-    set (krb5_src_SOURCE_DIR "${SOURCE_DIR}")
-    file (MAKE_DIRECTORY ${krb5_src_SOURCE_DIR}/include)
 
-    set_target_properties (krb5 PROPERTIES
-        IMPORTED_LOCATION
-          ${BINARY_DIR}/${ep_lib_prefix}uv.so.1
-        INTERFACE_INCLUDE_DIRECTORIES
-          ${SOURCE_DIR}/include)
-    add_dependencies(krb5 krb5_src)
 
-    file(TO_CMAKE_PATH "${krb5_src_SOURCE_DIR}" krb5_src_SOURCE_DIR)
-    target_include_directories (krb5 INTERFACE ${krb5_src_SOURCE_DIR}/include)
+
+    find_library(krb5 NAMES krb5 krb5-dev libkrb5 libkrb5-dev HINTS ${nih_cache_path}/src/krb5_src/lib/ REQUIRED)
+    message(${nih_cache_path})
+    message(${krb5})
+    message(krb5)
+
+    if(NOT krb5)
+        set(krb5found FALSE)
+        add_library(krb5 SHARED IMPORTED GLOBAL)
+        ExternalProject_Add(krb5_src
+            PREFIX ${nih_cache_path}
+            GIT_REPOSITORY https://github.com/krb5/krb5.git
+            GIT_TAG master
+            CONFIGURE_COMMAND autoreconf src && ./src/configure
+            BUILD_IN_SOURCE 1
+            BUILD_COMMAND make
+            INSTALL_COMMAND ""
+            )
+
+        ExternalProject_Get_Property (krb5_src SOURCE_DIR)
+        ExternalProject_Get_Property (krb5_src BINARY_DIR)
+        set (krb5_src_SOURCE_DIR "${SOURCE_DIR}")
+        file (MAKE_DIRECTORY ${krb5_src_SOURCE_DIR}/include)
+
+        message(${SOURCE_DIR})
+        message(${BINARY_DIR})
+        set_target_properties (krb5 PROPERTIES
+            IMPORTED_LOCATION
+              ${BINARY_DIR}/lib/${ep_lib_prefix}krb5.so
+            INTERFACE_INCLUDE_DIRECTORIES
+              ${SOURCE_DIR}/include)
+        add_dependencies(krb5 krb5_src)
+
+        file(TO_CMAKE_PATH "${krb5_src_SOURCE_DIR}" krb5_src_SOURCE_DIR)
+        target_include_directories (krb5 INTERFACE ${krb5_src_SOURCE_DIR}/include)
+    else()
+        set(krbfound TRUE)
+    endif()
+
 
     message("NOT")
     find_library(libuv1 NAMES uv1 libuv1 liubuv1-dev libuv1:amd64 REQUIRED)
@@ -98,12 +116,23 @@ if(NOT cassandra)
           ${SOURCE_DIR}/include)
     add_dependencies(cassandra cassandra_src)
     ExternalProject_Add_StepDependencies(cassandra_src build libuv1)
-    ExternalProject_Add_StepDependencies(cassandra_src build krb5)
 
     file(TO_CMAKE_PATH "${cassandra_src_SOURCE_DIR}" cassandra_src_SOURCE_DIR)
     target_include_directories (cassandra INTERFACE ${cassandra_src_SOURCE_DIR}/include)
     target_link_libraries(cassandra INTERFACE libuv1)
-    target_link_libraries(cassandra INTERFACE krb5)
+    if(${krb5} STREQUAL "krb5-NOT_FOUND")
+        message("krb5 not found")
+        target_link_libraries(cassandra INTERFACE krb5)
+    else()
+        message("krb5 found")
+        target_link_libraries(cassandra INTERFACE ${krb5})
+    endif()
+    if(NOT krb5)
+        ExternalProject_Add_StepDependencies(cassandra_src build krb5)
+    endif()
+    if(NOT zlib)
+        ExternalProject_Add_StepDependencies(cassandra_src build zlib)
+    endif()
     target_link_libraries(ripple_libs INTERFACE cassandra)
 else()
 
